d5927b1ffba8c0f7b20987fbff7d067ffa227d72
Albert <dzakhoev@bk.ru>
2025-09-22 21:39:32 +0300

    Add php structure and logic

--- processOrder.php
+++ processOrder.php
@@ -1,2 +1,23 @@
 <?php
-var_dump($_POST); // просто покажем, что прислала форма
+
+define('TIREPRICE', 3000);
+define('OILPRICE', 1800);
+define('SPARKPRICE', 370);
+define('TAX_RATE', 0.20);
+
+$t1 = (int)($_POST['tovar1'] ?? 0);
+$t2 = (int)($_POST['tovar2'] ?? 0);
+$t3 = (int)($_POST['tovar3'] ?? 0);
+
+$subtotal = $t1*TIREPRICE + $t2*OILPRICE + $t3*SPARKPRICE;
+$tax = $subtotal*TAX_RATE;
+$total = $subtotal + $tax;
+?>
+<!doctype html>
+<meta charset="utf-8">
+<h2>Результаты заказа</h2>
+<p>Шины: <?= $t1 ?>; Масло: <?= $t2 ?>; Свечи: <?= $t3 ?></p>
+<p>Итого без налога: <?= $subtotal ?> руб.</p>
+<p>Налог (<?= (int)(TAX_RATE*100) ?>%): <?= $tax ?> руб.</p>
+<p><b>К оплате: <?= $total ?> руб.</b></p>
+<p><a href="orderform.html">Назад</a></p>
